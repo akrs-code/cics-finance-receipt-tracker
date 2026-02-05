0ae0f42077b20d9ca7ee9c4db50309119479dbfe
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-05 18:22:53 +0800

    refactor: added item unit

--- frontend/components/ReceiptContent.jsx
+++ frontend/components/ReceiptContent.jsx
@@ -137,7 +137,7 @@ export const ReceiptContent = ({ data }) => {
                 {items.map((item, i) => (
                     <View key={i} style={styles.tableRow}>
                         <Text style={[styles.colParticular, { fontWeight: "normal" }]}>
-                            {item.quantity} {item.name}
+                           {item.quantity} {item.unit || 'pc'} {item.name}
                         </Text>
                         <Text style={[styles.colAmount, { fontWeight: "normal" }]}>
                             {(Number(item.amount) * Number(item.quantity)).toFixed(2)}
