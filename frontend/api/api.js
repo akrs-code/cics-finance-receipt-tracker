de8940a3630209aa3f9d0215218f0aa41619ea25
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-14 22:15:21 +0800

    refacotr: added api for settings

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -32,4 +32,12 @@ export const updateReceipt = (id, data, token) =>
 export const deleteReceipt = (id, token) =>
   api.delete(`/api/receipt/${id}`, attachToken(token));
 
+export const getSettings = (token) => api.get("/api/settings", attachToken(token));
+export const addCouncilMember = (data, token) => api.post("/api/settings/council-member", data, attachToken(token));
+export const deleteCouncilMember = (idx, token) => api.delete(`/api/settings/council-member/${idx}`, attachToken(token));
+export const addAudit = (data, token) => api.post("/api/settings/audit", data, attachToken(token));
+export const deleteAudit = (idx, token) => api.delete(`/api/settings/audit/${idx}`, attachToken(token));
+export const addUnit = (data, token) => api.post("/api/settings/unit", data, attachToken(token));
+export const deleteUnit = (unitName, token) => api.delete(`/api/settings/unit/${unitName}`, attachToken(token));
+
 export default api;
