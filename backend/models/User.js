190f18a8044a2e0232bd5fc668e2d428191d3ba5
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-25 18:45:13 +0800

    refactor: remove lowercase true

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, lowercase: true, required: true },
+    password: { type: String, required: true },
     role: { type: String, enum: ["admin"], default: "admin" }
 
 }, { timestamps: true })
